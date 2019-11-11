8b8a16bc4c683afaf05107dd6062dd4d372ac9fb
mkamranhamid <mkamranhamid@gmail.com>
2019-11-11 23:38:04 +0500

    font-awesome added; loop thru paintings;

--- src/components/Thumbnail/Thumbnail.js
+++ src/components/Thumbnail/Thumbnail.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faSave } from '@fortawesome/free-solid-svg-icons'
 
 
-function Thumbnail({ }) {
+function Thumbnail({ data:{image, title, publishedAt} }) {
+    console.log({ image, title, publishedAt })
     return (
         <div className="thumbnail">
             <div className="thumbnail-img">
-                <img src={`${process.env.PUBLIC_URL}/images/products/paint1.jpg`} />
+                <img className="img-thumbnail" src={image} />
             </div>
             <div className="thumbnail-info">
-                <div className="info-name">
-                    Paint Name
+                <div className="infos">
+                    <div className="info-name">
+                        {title}
+                    </div>
+                    <div className="info-published-date">
+                        {publishedAt}
+                    </div>
                 </div>
-                <div className="info-published-date">
-                    August 13, 2019
+                <div className="info-actions">
+                    <button><FontAwesomeIcon icon={faHeart} /></button>
+                    <button><FontAwesomeIcon icon={faSave} /></button>
                 </div>
             </div>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,37 @@ import { observer } from 'mobx-react-lite';
 
 import { Thumbnail } from '../components/Thumbnail';
 
+
 const HomePage = observer(() => {
+
+    const data = [
+        {
+            image: `${process.env.PUBLIC_URL}/images/products/paint1.jpg`,
+            title: "Snickers eShop",
+            publishedAt: "October 25, 2018",
+        },
+        {
+            image: `${process.env.PUBLIC_URL}/images/products/paint2.jpg`,
+            title: "Uruoi Japanese Skincare",
+            publishedAt: "August 13, 2019",
+        },
+        {
+            image: `${process.env.PUBLIC_URL}/images/products/paint3.jpg`,
+            title: "CoffeeStyle - Webflow",
+            publishedAt: "November 27, 2018",
+        },
+    ]
+    console.log(data);
     return (
         <div>
             <h1>HOME COMPONENT</h1>
-            <Thumbnail />
+            {
+
+                data.map((d, i) => (
+                    <Thumbnail data={d} key={i} />
+                ))
+            }
+
         </div>
     )
 })
